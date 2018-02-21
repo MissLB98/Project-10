765baba221fd858dbff925f77deb38e567f089f1
Leah Birch <goldbeat98@gmail.com>
2018-02-21 12:55:43 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $.ajax({
 					
 					
 				//Employee home city
-					HTML += '<span>' + data.results[i].location.city + '</span>';
+					HTML += '<p class="city"><span>City:</span> ' + data.results[i].location.city + '</p>';
 					
 				HTML += '</div>';
 				
@@ -33,27 +33,25 @@ $.ajax({
 				//Modal window info!!!!!!!!!!!!!
 				HTML += '<div class="modal-info">';//more contact
 				
-				//divider
-					HTML += '<hr class="divider"/>';
-				
 				//ID name
 					HTML += '<p><span>ID:</span> ' + data.results[i].id.name + '</p>';
 				
 				//Username
-					HTML += '<h3>' + data.results[i].login.username + '</h3>';
+					HTML += '<p><span>Username:</span> ' + data.results[i].login.username + '</p>';
 					
 				//Cell Number
-					HTML += '<p><a href="tel:' + data.results[i].cell + '">' + data.results[i].cell + '</a></p>';
+					HTML += '<p><span>Cell:</span> <a href="tel:' + data.results[i].cell + '">' + data.results[i].cell + '</a></p>';
+					
+				//DOB
+					HTML += '<p><span>DOB:</span> ' + data.results[i].dob + '</p>';
 					
 				//Address
-					HTML += '<h3>Address</h3>' +
+					HTML += '<div class="address"><h3>Address</h3>' +
 						'<p><span>Street Name:</span> ' + data.results[i].location.street + '</p>' +
-						'<p><span>City:</span> ' + data.results[i].location.city + '</p>' +
 						'<p><span>Country:</span> ' + data.results[i].location.state + '</p>' +
-						'<p><span>Postal Code:</span> ' + data.results[i].location.postcode + '</p>';
 						
-				//DOB
-					HTML += '<p><span>Birthday:</span> ' + data.results[i].dob + '</p>';
+						'<p><span>Postal Code:</span> ' + data.results[i].location.postcode + '</p></div>';
+						
 					
 				HTML += '</div>';
 				///////end of modal window
@@ -66,9 +64,43 @@ $.ajax({
 		$('#directory').html(HTML);
  
 	$('.employee').click( function() {
+		$('#modal').css({
+			'display' : 'block'
+		});
 		
+		$('#modalContent').html(this.innerHTML);
+		
+		$('.close').css({
+			'display' : 'block'
+		});
+		
+		$('.modal-info').css({
+			'display' : 'block'
+		});
+		
+		$('.employee .modal-info').css({
+			'display' : 'none'
+		});
+		
+		$('.employee .close').css({
+			'display' : 'none'
+		});
 	});
 		
+		
+	$('#modal').click( function() {
+		$('#modal').css({
+			'display' : 'none'
+		});
+		
+		$('.modal-info').css({
+			'display' : 'none'
+		});
+		
+		$('.close').css({
+			'display' : 'none'
+		});
+	});
 		
 	}	
 });
